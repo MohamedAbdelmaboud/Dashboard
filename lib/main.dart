5fcada0e0a7c8769376af529d96f603c6909ea42
MohamedAbdelmaboud <mohamedabdlmabod03@gmail.com>
2024-03-07 03:01:01 +0200

    add adptive layout

--- lib/main.dart
+++ lib/main.dart
@@ -21,48 +21,15 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             'Title text',
-            style: TextStyle(
-                fontSize: getResponsiveFontSize(context,
-                    fontSize: 20)), // fixed scale factor
-          ),
-          Text(
-            'This is the beginng of the app',
-            style: TextStyle(
-                fontSize: getResponsiveFontSize(context,
-                    fontSize: 16)), // fixed scale factor
-          ),
+          ), // fixed scale factor
         ],
       ),
     );
-  }
-}
-
-double getResponsiveFontSize(BuildContext context, {required double fontSize}) {
-  double scaleFactor = getScaleFactor(context);
-  double responsivefontSize = scaleFactor * fontSize;
-  //************/
-  double lowerLimit = responsivefontSize * 0.8; // less than 20 %
-  double upperLimit = responsivefontSize * 1.20; // larger than 20 %
-  double finalFontSize = responsivefontSize.clamp(lowerLimit, upperLimit);
-
-  print(
-      'the font size is $fontSize ◘◘ The responsivefontSize is $responsivefontSize ◘◘ upper: $upperLimit ◘◘ lowerLimit :$lowerLimit, the final font : $finalFontSize');
-  return finalFontSize;
-}
-
-double getScaleFactor(BuildContext context) {
-  double width = MediaQuery.sizeOf(context).width;
-  if (width < 600) {
-    return width / 400; // scale factor for mobile --> 400 reference width
-  } else if (width < 900) {
-    return width / 700; // scale factor for tablet --> 700 reference width
-  } else {
-    return width / 1100; // scale factor for desktop --> 1100  reference width
   }
 }
